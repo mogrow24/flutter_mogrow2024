44b4586a6dbc470cf09426533dffe11f7f6924d2
kjh <clsrn254@gmail.com>
2024-06-17 17:51:12 +0900

    Basic settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +14,29 @@ class MyApp extends StatelessWidget {
       home: Scaffold( // 화면 구성 및 구조에 관한 것 (appBar or body)
         appBar: AppBar(
           backgroundColor: Colors.blueGrey,
-          title: Text('Hello World!'),
+          title: Text('상단바'),
         ),
         body: Center(
-          child: Text('Hello World!!'),
+          child: Text('contents'),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: const [
+              // IconButton(
+              //     onPressed: () {
+              //
+              //     },
+              //     icon: SvgPicture.asset(
+              //       'assets/icons/home.svg',
+              //     ),
+              //     iconSize: 160,
+              // ),
+              Icon(Icons.star),
+              Icon(Icons.star),
+              Icon(Icons.star)
+            ],
+          ),
         ),
       ),
     );
